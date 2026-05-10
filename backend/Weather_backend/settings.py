f9134f2576789986b86cd419085534139d650240
saran <sarankumar10307@gmail.com>
2026-05-10 18:51:06 +0530

    backend deploy ready

--- backend/Weather_backend/settings.py
+++ backend/Weather_backend/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
