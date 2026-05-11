5b3f5319631b9ec0cdcd348e1bb3ac68033d8213
saran <sarankumar10307@gmail.com>
2026-05-11 08:41:43 +0530

    backend

--- backend/Weather_backend/settings.py
+++ backend/Weather_backend/settings.py
@@ -77,9 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Weather_backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
