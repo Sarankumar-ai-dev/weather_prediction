c9605e60e46d11fad76570626818da8b71a6a2c9
saran <sarankumar10307@gmail.com>
2026-05-11 18:12:57 +0530

    back

--- backend/weather_app/views.py
+++ backend/weather_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import date as dt_date
 from django.core.cache import cache
+import os
 
 
 @api_view(['GET'])
@@ -26,60 +27,32 @@ def get_weather(request):
 
     try:
 
-        # GEO API
-        geo_url = (
-            f"https://geocoding-api.open-meteo.com/v1/search?"
-            f"name={place}&count=1"
-        )
+        API_KEY = "cd271f231eac4499a2b120941261105"
 
-        geo_res = requests.get(
-            geo_url,
-            timeout=10,
-            headers={
-                "User-Agent": "Mozilla/5.0"
-            }
-        ).json()
-
-        if "results" not in geo_res:
-            return Response({
-                "error": "Place not found"
-            })
-
-        lat = geo_res["results"][0]["latitude"]
-        lon = geo_res["results"][0]["longitude"]
-
-        # WEATHER API
         weather_url = (
-            f"https://api.open-meteo.com/v1/forecast?"
-            f"latitude={lat}"
-            f"&longitude={lon}"
-            f"&hourly="
-            f"temperature_2m,"
-            f"precipitation,"
-            f"wind_speed_10m,"
-            f"relative_humidity_2m"
-            f"&forecast_days=7"
-            f"&timezone=auto"
+            f"http://api.weatherapi.com/v1/forecast.json?"
+            f"key={API_KEY}"
+            f"&q={place}"
+            f"&days=7"
+            f"&aqi=no"
+            f"&alerts=no"
         )
 
         weather_res = requests.get(
             weather_url,
-            timeout=10,
-            headers={
-                "User-Agent": "Mozilla/5.0"
-            }
+            timeout=10
         ).json()
 
         print(weather_res)
 
-        if "hourly" not in weather_res:
+        if "forecast" not in weather_res:
 
             return Response({
                 "error": "Weather data not available",
                 "api_response": weather_res
             })
 
-        hourly = weather_res["hourly"]
+        forecast_days = weather_res["forecast"]["forecastday"]
 
         result = []
 
@@ -88,25 +61,27 @@ def get_weather(request):
         winds = []
         humidity = []
 
-        for i in range(len(hourly["time"])):
+        for day in forecast_days:
 
-            t = hourly["temperature_2m"][i]
-            r = hourly["precipitation"][i]
-            w = hourly["wind_speed_10m"][i]
-            h = hourly["relative_humidity_2m"][i]
+            for hour in day["hour"]:
 
-            temps.append(t)
-            rains.append(r)
-            winds.append(w)
-            humidity.append(h)
+                t = hour["temp_c"]
+                r = hour["precip_mm"]
+                w = hour["wind_kph"]
+                h = hour["humidity"]
 
-            result.append({
-                "time": hourly["time"][i],
-                "temperature": t,
-                "rain": r,
-                "wind": w,
-                "humidity": h
-            })
+                temps.append(t)
+                rains.append(r)
+                winds.append(w)
+                humidity.append(h)
+
+                result.append({
+                    "time": hour["time"],
+                    "temperature": t,
+                    "rain": r,
+                    "wind": w,
+                    "humidity": h
+                })
 
         summary = {
             "avg_temp": round(sum(temps) / len(temps), 1),
